bc4ba91ccd61fb206185cd03b4b70a283874444c
patrick_w <patrick_w>
2003-10-11 19:53:33 +0000

    Using align flag, starting gdt creation.

--- dante/src/kernel/mem/init.C
+++ dante/src/kernel/mem/init.C
@@ -2,18 +2,19 @@
 #include <stdint.h>
 
 #include <display/textStream.h>
-uint32_t g_chunkOfMem[1024*3];
+uint32_t g_chunkOfMem[1024*3] __attribute__((__aligned__(4096)));
 uint32_t * g_pageDirectory;
+uint32_t * g_gdt;
 
 void initializePaging()
 {
 #define LOWPTR(x) ((uint32_t *) (((uint32_t) (x)) ^ 0xC0000000))
 //#define LOWPTR(x) ((uint32_t *) (x)) 
     uint32_t * l_pageTable;
-    l_pageTable = LOWPTR((((uint32_t) &g_chunkOfMem[0]) + 0x1000) 
-			       & (~0xFFF));
+    l_pageTable = LOWPTR(&g_chunkOfMem[0]);
     LOWPTR(&g_pageDirectory)[0]
 		= (uint32_t) (((uint32_t)(l_pageTable)) + 0x1000);
+    LOWPTR(&g_gdt)[0] = (uint32_t) (((uint32_t)(l_pageTable)) + 0x2000);
     
     l_pageTable[0] = 0 | 02; // Don't want to access page 0. (NULL)
     // set up rest of first 4MB.
@@ -26,13 +27,14 @@ void initializePaging()
 	(*(uint32_t **)LOWPTR(&g_pageDirectory))[i] = 0 | 02;
     (*(uint32_t **)LOWPTR(&g_pageDirectory))[0x0] = ((uint32_t)(l_pageTable)) | 03;
     (*(uint32_t **)LOWPTR(&g_pageDirectory))[0x300] = ((uint32_t)(l_pageTable)) | 03;
-
-
-    
+     
+    // start using page directory.
     register uint32_t reg_a = (uint32_t)(*LOWPTR(&g_pageDirectory));
     asm("mov %0, %%cr3" : :"r" (reg_a));
     asm("mov %%cr0, %0" : "=r" (reg_a) :);
     reg_a |= 0x80000000;
     asm("mov %0, %%cr0" : :"r" (reg_a));
+
+    
     return;
 }
